7af65b3166daa8a87e4e66eaa3d40d51bc03ed65
Fatih Berkant EREN <fatihberkant38@gmail.com>
2024-09-09 11:29:00 +0300

    Release - 3.0

---
+++ src/app/components/order-history/order-history.component.ts
@@ -0,0 +1,35 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { OrderHistory } from 'src/app/common/order-history';
+import { OrderHistoryService } from 'src/app/services/order-history.service';
+
+@Component({
+  selector: 'app-order-history',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './order-history.component.html',
+  styleUrl: './order-history.component.css'
+})
+export class OrderHistoryComponent implements OnInit {
+
+  orderHistoryList: OrderHistory[] = [];
+  storage: Storage = sessionStorage;
+
+  constructor(private orderHistoryService: OrderHistoryService) { }
+
+  ngOnInit(): void {
+    this.handleOrderHistory();
+  }
+  handleOrderHistory() {
+    
+    // read the user's email address from browser session
+    const theEmail = JSON.parse(this.storage.getItem('userEmail'));
+
+    // retrieve data from the service
+    this.orderHistoryService.getOrderHistory(theEmail).subscribe(
+      data => {
+        this.orderHistoryList = data._embedded.orders;
+      }
+    )
+  }
+}
